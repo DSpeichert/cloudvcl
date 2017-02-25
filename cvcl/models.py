0d48f359af79d05194199f2565a134d3fbd0bf6b
Nuyb <michael.s.byun@gmail.com>
2017-02-25 01:59:05 -0500

    Add models for User, User Group, and Assignment
    Also sets default timezone to America\New_York

--- cvcl/models.py
+++ cvcl/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model)
+    username = models.CharField(max_length=30)
+    user_group = models.ManyToManyField(UserGroup)
+
+    def __str__(self):
+    	return self.username
+
+class UserGroup(models.Model):
+    name = models.CharField(max_length=30)
+    instructor_id = models.IntegerField()
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+    	return self.name
+
+class assignment(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+    	return self.name
