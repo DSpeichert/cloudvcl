167cee3aec4c3398ba176f046eaf893c18e43268
Nuyb <michael.s.byun@gmail.com>
2017-03-01 18:54:15 -0500

    More models

--- cvcl/models.py
+++ cvcl/models.py
@@ -1,12 +1,14 @@
-from django.db import models
-from django.utils import timezone
+import uuid
 
-class User(models.Model)
+from django.db import models
+
+
+class User(models.Model):
     username = models.CharField(max_length=30)
-    user_group = models.ManyToManyField(UserGroup)
 
     def __str__(self):
-    	return self.username
+        return self.username
+
 
 class UserGroup(models.Model):
     name = models.CharField(max_length=30)
@@ -14,9 +16,10 @@ class UserGroup(models.Model):
     users = models.ManyToManyField(User)
 
     def __str__(self):
-    	return self.name
+        return self.name
 
-class assignment(models.Model):
+
+class Assignment(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     start_date = models.DateTimeField()
@@ -24,4 +27,58 @@ class assignment(models.Model):
     user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
 
     def __str__(self):
-    	return self.name
+        return self.name
+
+
+class Environment(models.Model):
+    environment_name = models.CharField(max_length=30)
+    status = models.CharField(max_length=30)
+    size = models.PositiveIntegerField()
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.environment_name
+
+
+class VM(models.Model):
+    instance_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image_name = models.CharField(max_length=30)
+    ip_address = models.GenericIPAddressField()
+    environments = models.ForeignKey(Environment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.instance_uuid
+
+
+class EnvironmentDefinition(models.Model):
+    disk = models.PositiveIntegerField()
+    cpu = models.PositiveIntegerField()
+    ram = models.PositiveIntegerField()
+    assignments = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.disk
+
+
+class Image(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return self.uuid
+
+
+class VmDefinition(models.Model):
+    user_script = models.TextField()
+    image = models.OneToOneField(Image, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user_script
+
+
+class Flavor(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    vm_definition = models.OneToOneField(VmDefinition, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.uuid
+
