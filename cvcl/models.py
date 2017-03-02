de91b4afeb16f478310200230e741689124ae3a7
Daniel Speichert <daniel@speichert.pl>
2017-03-01 22:41:52 -0500

    Fix models + add migrations

--- cvcl/models.py
+++ cvcl/models.py
@@ -1,10 +1,56 @@
-import uuid
-
 from django.db import models
+from django.utils import timezone
+
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField()
+    user_group = models.ForeignKey('UserGroup', on_delete=models.CASCADE)
+    environment_definition = models.ForeignKey('EnvironmentDefinition', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+# an instance of EnvironmentDefinition that exists in OpenStack cloud
+class Environment(models.Model):
+    assignment = models.ForeignKey('Assignment', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+
+class EnvironmentDefinition(models.Model):
+    def __str__(self):
+        return self.id
+
+
+# This is simply a reference to OpenStack Flavors. Only flavors represented by UUIDs here are available to users.
+class Flavor(models.Model):
+    uuid = models.UUIDField()
+
+    def __str__(self):
+        return self.uuid
+
+
+class Image(models.Model):
+    uuid = models.UUIDField()
+
+    def __str__(self):
+        return self.uuid
 
 
 class User(models.Model):
     username = models.CharField(max_length=30)
+    limit_instances = models.IntegerField(default=0)
+    limit_cpus = models.IntegerField(default=0)
+    limit_ram = models.IntegerField(default=0, verbose_name="RAM limit in bytes")
+    is_instructor = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    user_groups = models.ManyToManyField('UserGroup')
 
     def __str__(self):
         return self.username
@@ -12,56 +58,20 @@ class User(models.Model):
 
 class UserGroup(models.Model):
     name = models.CharField(max_length=30)
-    instructor_id = models.IntegerField()
-    users = models.ManyToManyField(User)
+    instructor = models.ForeignKey('User', on_delete=models.CASCADE, limit_choices_to={'is_instructor': True},
+                                   related_name='instructs')
+    users = models.ManyToManyField('User')
 
     def __str__(self):
-        return self.name
+        return self.id
 
 
-class Assignment(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class Environment(models.Model):
-    environment_name = models.CharField(max_length=30)
-    status = models.CharField(max_length=30)
-    size = models.PositiveIntegerField()
-    users = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.environment_name
-
-
-class VM(models.Model):
-    instance_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    image_name = models.CharField(max_length=30)
+# an instance of VMDefinition that exists in OpenStack cloud
+class Vm(models.Model):
+    uuid = models.UUIDField()
     ip_address = models.GenericIPAddressField()
-    environments = models.ForeignKey(Environment, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.instance_uuid
-
-
-class EnvironmentDefinition(models.Model):
-    disk = models.PositiveIntegerField()
-    cpu = models.PositiveIntegerField()
-    ram = models.PositiveIntegerField()
-    assignments = models.ForeignKey(Assignment, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.disk
-
-
-class Image(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    environment = models.ForeignKey('Environment', on_delete=models.CASCADE)
+    vm_definition = models.ForeignKey('VmDefinition')
 
     def __str__(self):
         return self.uuid
@@ -69,16 +79,8 @@ class Image(models.Model):
 
 class VmDefinition(models.Model):
     user_script = models.TextField()
-    image = models.OneToOneField(Image, on_delete=models.CASCADE, primary_key=True)
+    image = models.ForeignKey('Image', on_delete=models.CASCADE)
+    flavor = models.ForeignKey('Flavor', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user_script
-
-
-class Flavor(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    vm_definition = models.OneToOneField(VmDefinition, on_delete=models.CASCADE, primary_key=True)
-
-    def __str__(self):
-        return self.uuid
-
+        return self.id
