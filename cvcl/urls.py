f2dd4038d91668959b9595742dc4b37d389b3338
Abhishek Valanju <abhishekvalanju@users.noreply.github.com>
2017-03-10 02:26:34 -0500

    Master bootstrap template

--- cvcl/urls.py
+++ cvcl/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from . import views
+
 from cvcl.views import *
+from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),  # welcome page for all
-    url(r'^assignments$', AssignmentList.as_view()),  # student, instructor (list)
+    url(r'^assignments$', AssignmentList.as_view(), name='assignments'),  # student, instructor (list)
     # url(r'^environment/:id$', UserQuota.as_view()),  # student (sees environment)
     # url(r'^assignments/:id$', UserQuota.as_view()),  # instructor (edits assignment)
     # url(r'^assignments/create$', ),
