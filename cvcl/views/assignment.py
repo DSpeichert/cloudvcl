a9d6fd11d6e0afec987cfe7308a4b2a51f1e70b7
Daniel Speichert <daniel@speichert.pl>
2017-03-11 01:16:45 -0500

    Fix assignment view querysets

--- cvcl/views/assignment.py
+++ cvcl/views/assignment.py
@@ -21,21 +21,20 @@ class AssignmentCreate(LoginRequiredMixin, CreateView):
         return super(AssignmentCreate, self).form_valid(form)
 
 
-@method_decorator(user_passes_test(is_instructor_check), name='dispatch')
 class AssignmentDetail(LoginRequiredMixin, DetailView):
     template_name = './assignment_detail.html'
 
     def get_queryset(self):
-        return Assignment.objects.filter(course__in=self.request.user.instructs) | self.request.user.courses.filter(
-            start_date__gte=datetime.now(), end_date__lte=datetime.now())
+        return Assignment.objects.filter(course__in=self.request.user.instructs.all()) | Assignment.objects.filter(
+            course__in=self.request.user.courses.all(), start_date__gte=datetime.now(), end_date__lte=datetime.now())
 
 
 class AssignmentList(LoginRequiredMixin, ListView):
     template_name = './assignment_list.html'
 
     def get_queryset(self):
-        return Assignment.objects.filter(course__in=self.request.user.instructs) | self.request.user.courses.filter(
-            start_date__gte=datetime.now(), end_date__lte=datetime.now())
+        return Assignment.objects.filter(course__in=self.request.user.instructs.all()) | Assignment.objects.filter(
+            course__in=self.request.user.courses.all(), start_date__gte=datetime.now(), end_date__lte=datetime.now())
 
 
 @method_decorator(user_passes_test(is_instructor_check), name='dispatch')
@@ -44,7 +43,7 @@ class AssignmentUpdate(LoginRequiredMixin, UpdateView):
     fields = ['name', 'description', 'start_date', 'end_date', 'course', 'environment_definition']
 
     def get_queryset(self):
-        return Assignment.objects.filter(course__in=self.request.user.instructs)
+        return Assignment.objects.filter(course__in=self.request.user.instructs.all())
 
 
 @method_decorator(user_passes_test(is_instructor_check), name='dispatch')
@@ -53,4 +52,4 @@ class AssignmentDelete(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('assignments')
 
     def get_queryset(self):
-        return Assignment.objects.filter(course__in=self.request.user.instructs)
+        return Assignment.objects.filter(course__in=self.request.user.instructs.all())
