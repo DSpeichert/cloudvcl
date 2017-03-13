ecb0eb0b37ae096fdf669b21637e3beda1071eb4
Daniel Speichert <daniel@speichert.pl>
2017-03-13 17:36:10 -0400

    Admin: add column course to assignments

--- cvcl/admin.py
+++ cvcl/admin.py
@@ -2,13 +2,24 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Assignment)
 admin.site.register(Environment)
 admin.site.register(EnvironmentDefinition)
 admin.site.register(Flavor)
 admin.site.register(Image)
 admin.site.register(Vm)
 admin.site.register(VmDefinition)
+
+
+@admin.register(Assignment)
+class CustomAssignmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course',)
+    list_filter = ('course',)
+
+
+@admin.register(Course)
+class CustomCourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'instructor',)
+    list_filter = ('instructor',)
 
 
 @admin.register(User)
@@ -25,9 +36,3 @@ class CustomUserAdmin(UserAdmin):
                              }
          ),
     )
-
-
-@admin.register(Course)
-class CustomCourseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'instructor',)
-    list_filter = ('instructor',)
