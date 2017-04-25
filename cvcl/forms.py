7579dd395fbb9ef1f49390a555015393ffc13af5
Daniel Speichert <daniel@speichert.pro>
2017-04-24 20:59:16 -0400

    bootstrap-datetimepicker for assignment create/edit (#41)

--- cvcl/forms.py
+++ cvcl/forms.py
@@ -1,11 +1,16 @@
 from django.forms import ModelForm, ModelChoiceField
 from .models import Assignment, VmDefinition
+from bootstrap3_datetime.widgets import DateTimePicker
 
 
 class AssignmentForm(ModelForm):
     class Meta:
         model = Assignment
         fields = ['name', 'description', 'start_date', 'end_date', 'course', 'environment_definition']
+        widgets = {
+            'start_date': DateTimePicker(options={"format": "YYYY-MM-DD HH:mm:ss"}),
+            'end_date': DateTimePicker(options={"format": "YYYY-MM-DD HH:mm:ss"})
+        }
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)  # pop the 'user' from kwargs dictionary
